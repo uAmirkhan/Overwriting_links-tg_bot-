be9d0746c061082dd590e5e0bf7323853f5c7600
uAmirkhan <amrumaev@mail.ru>
2023-11-21 15:42:59 +0700

    bot can overwriting and delete message

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import json
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, TypeHandler
 
-def echo(update: Update, context: CallbackContext) -> None:
-    text = json.dumps(update.to_dict(),indent=2)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+def overwriting(update:Update,context: CallbackContext):
+    domen = "https://www."
+    if domen in update.message.text:
+        text = update.message.text.replace("www.","dd")
+        update.message.reply_text(text)
+        context.bot.delete_message(update.message.chat_id,update.message._id_attrs[0])
+    else:
+        update.message.reply_text("Это не сылка")
+        context.bot.delete_message(update.message.chat_id, update.message._id_attrs[0])
+
+
 def main():
     updater = Updater("6604273995:AAFUyB7klkCWh7MtzhfVOdfZCdnHjpbk7qw")
-    updater.dispatcher.add_handler(TypeHandler(Update,echo))
+    updater.dispatcher.add_handler(TypeHandler(Update,overwriting))
     updater.start_polling()
 
     print('Started')
